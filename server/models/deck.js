3741cf455bef7607f21411eb7f5e1db8fe09687f
Richard Nicholson <twilly0@gmail.com>
2018-06-26 15:53:15 -0700

    implemented shuffle

--- server/models/deck.js
+++ server/models/deck.js
@@ -22,7 +22,21 @@ Deck.methods.putOnBottom = function(card)
 
 Deck.methods.shuffle = function()
 {
-    // TODO implement shuffle
+    let currentIndex = this.cards.length;
+    let temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = this.cards[currentIndex];
+        this.cards[currentIndex] = this.cards[randomIndex];
+        this.cards[randomIndex] = temporaryValue;
+    }
 };
 
 module.exports = {Deck: Deck, deckSchema: deckSchema};
